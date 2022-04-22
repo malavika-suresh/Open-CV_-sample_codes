bcb7844c70ce97b83098c4967fabbcaee69ce4b6
Malavika Suresh <62960574+malavika-suresh@users.noreply.github.com>
2022-04-22 17:10:04 +0530

    Framing the mp4

---
+++ video2frames.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+cap=cv2.VideoCapture(r'20170505_102056.mp4')
+cpt=0
+while(cap.isOpened()):
+      ret,frame=cap.read()
+      gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+      cv2.imshow("frame",frame)
+      cv2.imwrite('E:\imageprocessing/img%04i.jpg'%cpt,frame)
+      cpt+=1
+      if cv2.waitKey(1)&0xFF==ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
