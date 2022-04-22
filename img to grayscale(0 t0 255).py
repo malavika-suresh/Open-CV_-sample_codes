f3c83dfe63f8e13d62f65e14d6d1a0df92d5c066
Malavika Suresh <62960574+malavika-suresh@users.noreply.github.com>
2022-04-22 17:14:04 +0530

    Create img to grayscale(0 t0 255).py

---
+++ img to grayscale(0 t0 255).py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img=cv2.imread(r'rose1.jpg')
+
+img1=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+cpt=0
+for thresh in range(0,255):
+	imbw=cv2.threshold(img1,thresh,255,cv2.THRESH_BINARY)
+cv2.imwrite(r'img%04i.jpg'%cpt,imbw)
+cpt+=1
+print("successfully completed")
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
